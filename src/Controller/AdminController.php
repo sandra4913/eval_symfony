cb7646349a45f932f8dabaeaff135b3c71276f50
Sandra Lebrin <sandra.lebrin@gmail.com>
2024-07-13 19:48:12 +0200

    Modify files

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Tickets;
+use App\Form\TicketsType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/admin/{id}', name: 'app_admin', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function index(?Tickets $tickets, Request $request, EntityManagerInterface $manager): Response
     {
+        $tickets ??= new Tickets();
+        $form = $this->createForm(TicketsType::class, $tickets);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($tickets);
+            $manager->flush();
+            return $this->redirectToRoute(route: 'app_list');
+        }
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'form' => $form,
         ]);
     }
 }
--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -6,16 +6,18 @@ use App\Repository\TicketsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ListController extends AbstractController
 {
+    #[IsGranted('IS_AUTHENTICATED')]
     #[Route('/list', name: 'app_list')]
     public function index(TicketsRepository $repository): Response
     {
         $tickets = $repository->findAll();
         return $this->render('list/index.html.twig', [
             'controller_name' => 'ListController',
-            'tickets' => $tickets
+            'tickets' => $tickets,
         ]);
     }
 }
